c0311723be90e6b06031b9d4b7c45ce36e4bb19e
nimrod321 <nimrodal@campus.technion.ac.il>
2026-06-01 19:10:10 +0300

    tahel mui bad

--- bp.cpp
+++ bp.cpp
@@ -2,9 +2,38 @@
 /* This file should hold your implementation of the predictor simulator */
 
 #include "bp_api.h"
+#include <stdio>
+#include <cmath>
+#include <cstdint>
+#include <vector>
 
+using std::vector;
+//tahel is bad
+class BTBEntry{
+	public:
+	bool valid = false;
+    uint32_t tag = 0;
+    uint32_t target = 0;
+    uint8_t local_history = 0;
+    std::vector<int> local_fsm_table;
+	};
+class BranchPredictor{
+	private:
+		unsigned btbSize;
+		unsigned historySize;
+		unsigned tagSize;
+		unsigned fsmState;
+		bool isGlobalHist;
+		bool isGlobalTable;
+		int Shared;
+		vector<int> global_fsm_table;
+		vector<BTBEntry> btb_table;
+	public:
+
+
+}
 int BP_init(unsigned btbSize, unsigned historySize, unsigned tagSize, unsigned fsmState,
-			bool isGlobalHist, bool isGlobalTable, int Shared){
+	bool isGlobalHist, bool isGlobalTable, int Shared){
 	return -1;
 }
 
@@ -19,4 +48,3 @@ void BP_update(uint32_t pc, uint32_t targetPc, bool taken, uint32_t pred_dst){
 void BP_GetStats(SIM_stats *curStats){
 	return;
 }
-
